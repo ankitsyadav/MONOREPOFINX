1daa722cc489428793bcd24f28c0fc7c3d2c7a28
ankitsinghyadavofficial <ankitsinghyadav.official@gmail.com>
2026-02-24 02:09:35 +0530

    Latest Deploytment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import app from './app.js';
 import { startCampaignScheduler } from './modules/campaign/campaign.scheduler.js';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   startCampaignScheduler();
